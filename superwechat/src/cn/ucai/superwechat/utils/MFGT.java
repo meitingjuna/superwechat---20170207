7f97cea73542a0cf8f5450aa2d7fa6eeed057db4
meitingjun <1271888057@qq.com>
2017-02-15 15:13:51 +0800

    点击联系人进入到详情资料界面和点击发消息跳转到聊天界面以及Title的修改

--- superwechat/src/cn/ucai/superwechat/utils/MFGT.java
+++ superwechat/src/cn/ucai/superwechat/utils/MFGT.java
@@ -12,6 +12,7 @@ import java.util.ArrayList;
 import cn.ucai.superwechat.R;
 import cn.ucai.superwechat.ui.AddContactActivity;
 import cn.ucai.superwechat.ui.AddFirentActivity;
+import cn.ucai.superwechat.ui.ChatActivity;
 import cn.ucai.superwechat.ui.FirentProfileActivity;
 import cn.ucai.superwechat.ui.GuideActivity;
 import cn.ucai.superwechat.ui.LoginActivity;
@@ -91,6 +92,11 @@ public class MFGT {
     public static void gotoAddFirent(Activity activity, String username) {
         startActivity(activity,new Intent(activity,AddFirentActivity.class).
         putExtra(I.User.USER_NAME,username));
+    }
+
+    public static void gotoChat(Activity activity, String mUserName) {
+        startActivity(activity ,new Intent(activity, ChatActivity.class)
+        .putExtra("userId",mUserName));
     }
 }
 
